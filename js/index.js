4bbf5d1b406ed5ef8ee721dfa050fa3e99de5df9
liwei <425528389@qq.com>
2019-05-14 21:08:37 +0800

    da

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@
                 txt+=['<div class="row text-center">',
                 '                <div class="col-md-4">',
                 '                    <fieldset class="fieldset">',
-                '                        <legend>行业："'+data.baiqi[i][0].hangye+'"</legend>',
+                '                        <legend>行业：'+data.baiqi[i][0].hangye+'</legend>',
                 '                        <div class="content">',
                 '                            <div class="top clearfix">',
                 '                                <div class="left">',
@@ -26,13 +26,14 @@
                 '                            </div>',
                 '                            <div class="bottom">',
                 '                                <p>地址：'+data.baiqi[i][0].address+'</p>',
+                '                                <p>公司：'+data.baiqi[i][0].company+'</p>',
                 '                            </div>',
                 '                        </div>',
                 '                    </fieldset>',
                 '                </div>',
                 '                <div class="col-md-4">',
                 '                    <fieldset class="fieldset">',
-                '                        <legend>行业："'+data.baiqi[i][1].hangye+'"</legend>',
+                '                        <legend>行业：'+data.baiqi[i][1].hangye+'</legend>',
                 '                        <div class="content">',
                 '                            <div class="top clearfix">',
                 '                                <div class="left">',
@@ -48,13 +49,14 @@
                 '                            </div>',
                 '                            <div class="bottom">',
                 '                                <p>地址：'+data.baiqi[i][1].address+'</p>',
+                '                                <p>公司：'+data.baiqi[i][1].company+'</p>',
                 '                            </div>',
                 '                        </div>',
                 '                    </fieldset>',
                 '                </div>',
                 '                <div class="col-md-4">',
                 '                    <fieldset class="fieldset">',
-                '                        <legend>行业："'+data.baiqi[i][2].hangye+'"</legend>',
+                '                        <legend>行业：'+data.baiqi[i][2].hangye+'</legend>',
                 '                        <div class="content">',
                 '                            <div class="top clearfix">',
                 '                                <div class="left">',
@@ -70,6 +72,7 @@
                 '                            </div>',
                 '                            <div class="bottom">',
                 '                                <p>地址：'+data.baiqi[i][2].address+'</p>',
+                '                                <p>公司：'+data.baiqi[i][2].company+'</p>',
                 '                            </div>',
                 '                        </div>',
                 '                    </fieldset>',
@@ -87,12 +90,28 @@
         $('#servicesinfo .container').append(txt); 
 
         
-        $('#servicesinfo .container .row').click(function (e) { 
+        $('#servicesinfo .container .row .col-md-4').click(function (e) { 
+           var baiqi_cross=$(this).parent().index()
+           var baiqi_vertical=$(this).index()
+           var referred=$('.intro-lead-in span,.navbar-nav li:nth-child(4) a span,.navbar-nav li:nth-child(6) a span')
 
-            console.log(31,$(this).index())
-            console.log(31,$(this).find('.fieldset').index())
+          /* 公司详情 */
+           //电话
+           var datainfo=data.baiqi[baiqi_cross][baiqi_vertical];
+           var phone=datainfo.phone;
+          $('.navbar-nav li:nth-child(7)').html('<a class="" href="tel:'+phone+'">电话：<span>'+phone+'</span></a>');
 
-      
+           //公司简称
+           referred.text(datainfo.Referred);
+           //公司介绍
+           $('.mission').text(datainfo.mission);
+           $('.slogan').text(datainfo.slogan);
+           $('.objective').text(datainfo.objective);
+           $('.slogan').text(datainfo.slogan);
+
+
+
+            company()
             
 
             $('.contentinfo').hide();
@@ -102,5 +121,27 @@
             e.preventDefault();
             
         });
+        $('#pageLimit').bootstrapPaginator({
+            currentPage: 1,//当前的请求页面。
+            totalPages: 20,//一共多少页。
+            size:"normal",//应该是页眉的大小。
+            bootstrapMajorVersion: 3,//bootstrap的版本要求。
+            alignment:"right",
+            numberOfPages:5,//一页列出多少数据。
+            itemTexts: function (type, page, current) {//如下的代码是将页眉显示的中文显示我们自定义的中文。
+                switch (type) {
+                case "first": return "首页";
+                case "prev": return "上一页";
+                case "next": return "下一页";
+                case "last": return "末页";
+                case "page": return page;
+                }
+            }
+        });
     
-	});
+    });
+    
+   
+function company(params) {
+    
+}
